7022dd323477712f6392f5cf43ab80cd6a68ad92
ThorDharanishs <dharanishsomu18@gmail.com>
2025-12-11 08:47:23 +0530

    Day 3 frontend

---
+++ React-frontend/client/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Greeting from './Greeting'
+import Counter from './Counter'
+function App() {
+ 
+
+  return (
+    <>
+    <h1>welcome</h1>
+    <Greeting name="Thor" />
+    <Counter />
+      
+    </>
+  )
+}
+
+export default App
---
+++ React-frontend/client/src/Greeting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Greeting = ({name})=>{
+    return (
+        <h1>{name}</h1>
+    )
+}
+
+export default Greeting
